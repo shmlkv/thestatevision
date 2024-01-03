2916a6eca418fe6ef337738cda0f3bf7b99823bc
sh-mimesis <shmelkov@mimesislabs.ai>
2024-01-03 18:29:44 +0000

    Add .nvmrc, update .gitignore, and modify server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,5 +4,5 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("STRAPI_URL", "https://thestatevision-c7215cb91521.herokuapp.com"),
+  url: env("STRAPI_URL"),
 });
