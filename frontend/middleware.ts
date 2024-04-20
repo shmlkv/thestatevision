9932bc680b194b6df9a623e16cfb58e5f61f65d1
shmlkv <dc@shmlkv.space>
2024-04-20 20:55:03 +0100

    Refactor middleware.ts: Remove unused import and simplify code

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -17,14 +17,15 @@ function getLocale(request: NextRequest): string | undefined {
   return matchLocale(languages, locales, i18n.defaultLocale);
 }
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  // const user = await getUserMeLoader(); // !!!
+  // const user = await getUserMeLoader();
   const currentPath = request.nextUrl.pathname;
 
   // if (currentPath.startsWith("/dashboard") && user.ok === false) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
+  // return NextResponse.redirect(new URL("/signin", request.url));
   // }
+  // console.log({ user });
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
   if (
@@ -37,7 +38,22 @@ export async function middleware(request: NextRequest) {
     ].includes(pathname)
   )
     return;
-  return NextResponse.next();
+
+  // Check if there is any supported locale in the pathname
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url)
+    );
+  }
 }
 
 export const config = {
