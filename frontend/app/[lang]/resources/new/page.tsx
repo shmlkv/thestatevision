ec3583c614af1b0f835d476a92d20d055261b7f9
shmlkv <work@shmlkv.space>
2024-05-02 12:00:25 +0100

    Add new resource page and update resources page

--- frontend/app/[lang]/resources/new/page.tsx
+++ frontend/app/[lang]/resources/new/page.tsx
@@ -11,26 +11,33 @@ export default async function Resources() {
   return (
     <div className="w-full">
       <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4">
-        Resources
+        Add new resource
       </h1>
       <div className="w-full space-y-2 flex flex-col">
-        <div className="flex space-x-2">
-          <Input required placeholder="Title" className="w-full" />
-          <Input required placeholder="URL" type="url" />
-        </div>
+        {/* <div className="flex space-x-2"> */}
+        <div className="text-sm font-semibold text-slate-400">Title *</div>
+
+        <Input required placeholder="Title" className="w-full" />
+        <div className="text-sm font-semibold text-slate-400">URL *</div>
+        <Input required placeholder="URL" type="url" />
+        {/* </div> */}
+        <div className="text-sm font-semibold text-slate-400">Text</div>
         <Textarea placeholder="Text" />
-        <Button
-          variant={"secondary"}
-          onClick={() =>
-            toast({
-              title: "Not yet",
-              description: "Your resource has been submitted",
-              duration: 5000,
-            })
-          }
-        >
-          Submit
-        </Button>
+        <div className="flex w-full">
+          <Button
+            className="mt-2 w-full"
+            variant={"secondary"}
+            onClick={() =>
+              toast({
+                title: "Not yet",
+                description: "Your resource has been submitted",
+                duration: 5000,
+              })
+            }
+          >
+            Submit
+          </Button>
+        </div>
       </div>
     </div>
   );
